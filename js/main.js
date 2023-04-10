ce056a0d85b6a823bc5161223970db593eddf1d8
leandrolinkolnr <100631881+leandrolinkolnr@users.noreply.github.com>
2023-04-10 17:36:17 -0300

    Removendo um Item da lista

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ form.addEventListener("submit", (evento) => {
         "prioridade": prioridade.value
     }
 
+    // Operador ternario: Se itens existir, ele coloca o id = id do ultimo+1
+    // Se ele não existir, coloca o id do itemAtual = 0
+    itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+
     criaTarefa(itemAtual)
 
     itens.push(itemAtual)
@@ -28,8 +32,6 @@ form.addEventListener("submit", (evento) => {
     prioridade.value = ""
 
 })
-
-
 
 function criaTarefa(item){
     // Exemplo de um item no html
@@ -48,7 +50,8 @@ function criaTarefa(item){
             // adicionou o numero prioridade ao numeroItem
     numeroItem.innerHTML = item.prioridade    
     
-   
+                        //
+    numeroItem.dataset.id = item.id
 
     //  Para colocar um elemento dentro do outro (numeroItem em novoItem),
     //     eu preciso da funcao appendChild
@@ -57,8 +60,35 @@ function criaTarefa(item){
             //Adicionando o nome ao novoItem
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDelete(item.id))
 
     // Pegando a lista declarada la em cima e adicionando o novoItem
     lista.appendChild(novoItem)
 
+}
+
+
+
+function botaoDelete(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = 'X'
+
+    elementoBotao.addEventListener("click", function() {
+        deletaTarefa(this.parentNode, id)
+                            //ParentNode, ele vai excluir o parente pai = tag <li>
+                            // Sem isso, ele excluia apenas o botao
+    })
+
+    return elementoBotao
+}
+
+
+
+
+function deletaTarefa(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
